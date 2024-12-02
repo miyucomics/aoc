942bb20b0f0a674ae989876ebd5e5de1a69d6c5c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-12-02 16:34:05 -0500

    2024 day 2

---
+++ 2024/day02/part2.py
@@ -0,0 +1,20 @@
+def isSafe(row):
+    signs = []
+    for i in range(len(row) - 1):
+        difference = row[i + 1] - row[i]
+        if abs(difference) > 3 or difference == 0:
+            return False
+        signs.append(difference > 0)
+    return all(signs) or not any(signs)
+
+def lenientSafety(row):
+    return any(
+        isSafe(row[:i] + row[i + 1:])
+        for i in range(len(row))
+    )
+
+with open("input.txt", "r") as file:
+    print(sum(
+        lenientSafety(list(map(int, line.split(" "))))
+        for line in file.read().splitlines()
+    ))
