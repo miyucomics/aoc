4e10165b5f56f4d8f815cc47e2215d9512bed348
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-12-03 18:05:40 -0500

    Minor update

--- 2024/day03/part1.py
+++ 2024/day03/part1.py
@@ -1,10 +1,8 @@
 import re
 
-expression = re.compile("mul\\(\\d+,\\d+\\)")
-
 with open("input.txt") as file:
     print(sum(
         int(a[4:]) * int(b[:-1])
-        for match in expression.findall("mul\\(\\d+,\\d+\\)", file.read())
+        for match in re.findall("mul\\(\\d+,\\d+\\)", file.read())
         for a, b in [match.split(",")]
     ))
