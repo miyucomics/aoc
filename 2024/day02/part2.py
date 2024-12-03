1596c7d8ece5e4203fcd449eca003fae1d8d5cfc
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-12-02 21:04:52 -0500

    2024 day 2 v2

--- 2024/day02/part2.py
+++ 2024/day02/part2.py
@@ -1,20 +1,17 @@
-def is_safe(row):
-    signs = []
-    for i in range(len(row) - 1):
-        difference = row[i + 1] - row[i]
-        if abs(difference) > 3 or difference == 0:
-            return False
-        signs.append(difference > 0)
-    return all(signs) or not any(signs)
-
-def lenient_check(row):
-    return any(
-        is_safe(row[:i] + row[i + 1:])
-        for i in range(len(row))
-    )
+# a different approach, assume that it's in descending order, reverse will catch other case
+def is_safe(data, tolerate_error):
+    for i in range(len(data) - 1):
+        if not 1 <= data[i] - data[i + 1] <= 3:
+            return tolerate_error and (
+                # no more error tolerance from here
+                is_safe(data[i - 1:i] + data[i + 1:], False) or  # remove current value
+                is_safe(data[i:i + 1] + data[i + 2:], False)     # remove next value
+            )
+    return True
 
 with open("input.txt", "r") as file:
-    print(sum(
-        lenient_check(list(map(int, line.split(" "))))
-        for line in file.read().splitlines()
-    ))
+    answer = 0
+    for line in file.read().splitlines():
+        line = list(map(int, line.split(" ")))
+        answer += is_safe(line, True) or is_safe(line[::-1], True)
+    print(answer)
