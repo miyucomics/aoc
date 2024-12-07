c633b4eb38f08bddd0a25fc3985dc6a4a3d3e960
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-12-07 09:25:25 -0500

    2024 day 7

---
+++ 2024/day07/part1.py
@@ -0,0 +1,23 @@
+def possible(target, elements, accumulator, index):
+    if index == len(elements):
+        return target == accumulator
+
+    if accumulator > target:
+        return False
+
+    if possible(target, elements, accumulator + elements[index], index + 1):
+        return True
+
+    if possible(target, elements, accumulator * elements[index], index + 1):
+        return True
+
+    return False
+
+with open("input.txt") as file:
+    answer = 0
+    for line in file.read().splitlines():
+        target, elements = line.split(": ")
+        target = int(target)
+        elements = list(map(int, elements.split(" ")))
+        answer += target * possible(target, elements, elements[0], 1)
+    print(answer)
