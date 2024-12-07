4b7310b08568960ce0346af6849023ee51b47c7c
miyucomics <nbriannyc@gmail.com>
2024-12-07 09:54:10 -0500

    2024 day 7 optimization

---
+++ 2024/day07/opt_part2.py
@@ -0,0 +1,26 @@
+def possible(target, elements):
+    if len(elements) == 0:
+        return target == 0
+
+    focus = elements[-1]
+
+    if target >= focus and possible(target - focus, elements[:-1]):
+        return True
+
+    if target % focus == 0 and possible(target / focus, elements[:-1]):
+        return True
+
+    digits = 10 ** len(str(focus))
+    if target % digits == focus and possible(target // digits, elements[:-1]):
+        return True
+
+    return False
+
+with open("input.txt") as file:
+    answer = 0
+    for line in file.read().splitlines():
+        target, elements = line.split(": ")
+        target = int(target)
+        elements = list(map(int, elements.split(" ")))
+        answer += target * possible(target, elements)
+    print(answer)
