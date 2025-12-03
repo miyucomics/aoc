6953478dd8c7abd089c398a3b544dc427f86e3c2
miyucomics <nbriannyc@gmail.com>
2025-12-03 00:37:53 -0500

    2025 day 3 edit part 1 for consistency

--- 2025/day03/part1.py
+++ 2025/day03/part1.py
@@ -3,17 +3,17 @@ with open("input.txt") as file:
 
 answer = 0
 for line in lines:
-    rep = list(map(int, line.strip()))
+    buffer = list(map(int, line.strip()))
 
-    first_pass_max = rep[0]
+    first_pass_max = buffer[0]
     first_pass_index = 0
-    for i, num in enumerate(rep):
-        if num > first_pass_max and i != len(rep) - 1:
+    for i, num in enumerate(buffer):
+        if num > first_pass_max and i != len(buffer) - 1:
             first_pass_index = i
             first_pass_max = num
 
     second_pass_max = float("-inf")
-    for i, num in enumerate(rep[first_pass_index + 1:]):
+    for i, num in enumerate(buffer[first_pass_index + 1:]):
         if num > second_pass_max:
             second_pass_max = num
 
