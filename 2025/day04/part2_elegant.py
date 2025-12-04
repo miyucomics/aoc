a3664c8196656f46a6ecd711fcd0f9c33752b2f1
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-12-04 00:54:33 -0500

    2025 day 4 optimization

---
+++ 2025/day04/part2_elegant.py
@@ -0,0 +1,46 @@
+with open("input.txt") as file:
+    world = file.read()
+    world_width = world.find("\n")
+    world_height = world.count("\n")
+    world = world.replace("\n", "")
+    world = list(map(lambda x: x == "@", world))
+
+def get(location):
+    if location.real < 0 or location.real >= world_width or location.imag < 0 or location.imag >= world_height:
+        return False
+    return world[int(world_width * location.imag + location.real)]
+
+neighbor_map = [0] * world_width * world_height
+def modify_neighbors(location, change):
+    if location.real < 0 or location.real >= world_width or location.imag < 0 or location.imag >= world_height:
+        return False
+    neighbor_map[int(world_width * location.imag + location.real)] += change
+
+offsets = {
+    y * 1j + x
+    for x in range(-1, 2)
+    for y in range(-1, 2)
+    if not (x == 0 and y == 0)
+}
+
+actives = {y * 1j + x for x in range(world_width) for y in range(world_height) if get(y * 1j + x)}
+for active in actives:
+    for offset in offsets:
+        modify_neighbors(active + offset, 1)
+
+answer = 0
+while True:
+    removable = [
+        active
+        for active in actives
+        if neighbor_map[int(active.imag * world_width + active.real)] < 4
+    ]
+    if len(removable) == 0:
+        break
+    for el in removable:
+        actives.remove(el)
+        for offset in offsets:
+            modify_neighbors(el + offset, -1)
+    answer += len(removable)
+
+print(answer)
