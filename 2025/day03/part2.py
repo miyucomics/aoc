46f820d84ac4200e3cb8722a948de96cec773662
miyucomics <nbriannyc@gmail.com>
2025-12-03 19:02:07 -0500

    2025 day 3 light optimization

--- 2025/day03/part2.py
+++ 2025/day03/part2.py
@@ -2,25 +2,27 @@ with open("input.txt") as file:
     lines = file.readlines()
 
 answer = 0
-passes = 12
+digits = 12
 
 for line in lines:
-    number_in_building = 0
-    passes_remaining = passes
+    constructed_number = 0
+    digits_left = digits
     buffer = list(map(int, line.strip()))
 
-    for i in range(passes):
-        passes_remaining -= 1
+    for i in range(digits):
+        digits_left -= 1
         pass_max = buffer[0]
         best_index = 0
 
-        for chosen_pos, chosen_digit in enumerate(buffer):
-            if chosen_digit > pass_max and len(buffer[chosen_pos + 1:]) >= passes_remaining:
+        end = len(buffer) - digits_left
+        for chosen_pos, chosen_digit in enumerate(buffer[:end]):
+            if chosen_digit > pass_max:
                 best_index = chosen_pos
                 pass_max = chosen_digit
-        buffer = buffer[best_index + 1:]
-        number_in_building = number_in_building * 10 + pass_max
 
-    answer += number_in_building
+        buffer = buffer[best_index + 1:]
+        constructed_number = constructed_number * 10 + pass_max
+
+    answer += constructed_number
 
 print(answer)
