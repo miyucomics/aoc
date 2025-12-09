4a4de3c33dcace9426c0709d736620da8767ea48
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-12-09 18:38:38 -0500

    2025 day 9 finish

--- 2025/day09/part1.py
+++ 2025/day09/part1.py
@@ -1,13 +1,13 @@
 with open("input.txt") as file:
-    red_tiles = []
+    points = []
     for line in file.readlines():
         a, b = line.split(",")
-        red_tiles.append(int(a) + int(b) * 1j)
+        points.append(int(a) + int(b) * 1j)
 
 answer = 0
-for i, a in enumerate(red_tiles):
-    for j in range(i + 1, len(red_tiles)):
-        b = red_tiles[j]
+for i, a in enumerate(points):
+    for j in range(i + 1, len(points)):
+        b = points[j]
         offset = a - b
         answer = max(answer, abs((offset.real + 1) * (offset.imag + 1)))
 
--- 2025/day09/part2.py
+++ 2025/day09/part2.py
@@ -1,33 +1,76 @@
+from collections import deque
+
 with open("input.txt") as file:
-    red_tiles = []
+    points = []
     for line in file.readlines():
-        a, b = line.split(",")
-        red_tiles.append((int(a), int(b)))
+        x, y = line.split(",")
+        points.append((int(x), int(y)))
 
-boundary_segments = []
-for i in range(len(red_tiles)):
-    p1 = red_tiles[i]
-    p2 = red_tiles[(i + 1) % len(red_tiles)]
-    boundary_segments.append((p1, p2))
+x_values = sorted({x for x, _ in points})
+y_values = sorted({y for _, y in points})
+x_to_comp = {val: i * 2 for i, val in enumerate(x_values)}
+y_to_comp = {val: i * 2 for i, val in enumerate(y_values)}
 
-def is_inside(point):
-    x, y = point
-    crossings = 0
+grid_width = len(x_values) * 2 - 1
+grid_height = len(y_values) * 2 - 1
+grid = [[1] * grid_height for _ in range(grid_width)]
 
-    for segment in boundary_segments:
-        a, b = segment
-        x1, y1 = a
-        x2, y2 = b
+loop_points = points + points[:1]
+for i in range(len(points)):
+    (ax, ay) = loop_points[i]
+    (bx, by) = loop_points[i + 1]
+    comp_ax, comp_ay = x_to_comp[ax], y_to_comp[ay]
+    comp_bx, comp_by = x_to_comp[bx], y_to_comp[by]
+    for x in range(min(comp_ax, comp_bx), max(comp_ax, comp_bx) + 1):
+        for y in range(min(comp_ay, comp_by), max(comp_ay, comp_by) + 1):
+            grid[x][y] = 0
 
-        if x1 == x2 and x == x1 and min(y1, y2) <= y <= max(y1, y2):
-            return True
-        if y1 == y2 and y == y1 and min(x1, x2) <= x <= max(x1, x2):
-            return True
+outside_region = {(-1, -1)}
+queue = deque(outside_region)
 
-        if x1 == x2:
-            if x < x1 and min(y1, y2) < y < max(y1, y2):
-                crossings += 1
+while queue:
+    x, y = queue.popleft()
+    for dx, dy in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+        if dx < -1 or dy < -1 or dx > len(grid) or dy > len(grid[0]):
+            continue
+        if 0 <= dx < len(grid) and 0 <= dy < len(grid[0]) and grid[dx][dy] == 0:
+            continue
+        if (dx, dy) in outside_region:
+            continue
+        outside_region.add((dx, dy))
+        queue.append((dx, dy))
 
-    return crossings % 2 == 1
+for x in range(len(grid)):
+    for y in range(len(grid[0])):
+        if (x, y) not in outside_region:
+            grid[x][y] = 0
 
-print(is_inside((0, 3)))
+prefix_sum = [[0] * len(row) for row in grid]
+for x in range(len(prefix_sum)):
+    for y in range(len(prefix_sum[0])):
+        prefix_sum[x][y] = (
+            grid[x][y]
+            + (prefix_sum[x - 1][y] if x > 0 else 0)
+            + (prefix_sum[x][y - 1] if y > 0 else 0)
+            - (prefix_sum[x - 1][y - 1] if x > 0 and y > 0 else 0)
+        )
+
+rectangles = []
+for i, (ax, ay) in enumerate(points):
+    for j in range(i):
+        (bx, by) = points[j]
+        area = (abs(ax - bx) + 1) * (abs(ay - by) + 1)
+        rectangles.append((area, ax, ay, bx, by))
+
+rectangles.sort(reverse=True)
+for area, ax, ay, bx, by in rectangles:
+    comp_ax, comp_ay = x_to_comp[ax], y_to_comp[ay]
+    comp_bx, comp_by = x_to_comp[bx], y_to_comp[by]
+    if (
+        prefix_sum[comp_bx][comp_by]
+        - (prefix_sum[comp_ax - 1][comp_by] if comp_ax > 0 else 0)
+        - (prefix_sum[comp_bx][comp_ay - 1] if comp_ay > 0 else 0)
+        + (prefix_sum[comp_ax - 1][comp_ay - 1] if comp_ax > 0 and comp_ay > 0 else 0)
+    ) == 0:
+        print(area)
+        exit()
